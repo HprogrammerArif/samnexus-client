b9d090d48a005146ea195b6e7e22b4b283936732
HprogrammerArif <programmerhabluarif@gmail.com>
2025-06-03 21:19:03 +0600

    first commit

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,54 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import axios from "axios";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+app.use(cookieParser());
+app.set("trust proxy", 1); // trust first proxy
+
+// apply rate limiting to all requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100), // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: true, // Disable the `X-RateLimit-*` headers
+  keyGenerator:(req: any) => req.ip
+});
+
+app.use(limiter);
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
+});
+
+// Proxy requests to the auth service
+app.use("/", proxy("http://localhost:6001"));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on("error", console.error);
